8e2d3cb42d44051a92c70d5e632ca5cdbb72e075
dy <dyu32345@gmail.com>
2024-09-15 21:12:22 +0800

    使用 Lock 来实现

--- thread01/src/main/java/com/dy/thread/sellticket/SellTicket.java
+++ thread01/src/main/java/com/dy/thread/sellticket/SellTicket.java
@@ -1,6 +1,8 @@
 package com.dy.thread.sellticket;
 
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @Author: dy
@@ -10,29 +12,29 @@ import java.util.Objects;
 public class SellTicket implements Runnable {
     private static int ticket = 100;
     private static Object obj = new Object();
+    private Lock lock = new ReentrantLock();
 
     @Override
     public void run() {
         while (true) {
+            lock.lock();
 
-            if (method()) break;
-
-        }
-    }
-
-    private synchronized static boolean method() {
-        if (ticket <= 0) {
-            System.out.println("电影票出售完毕!");
-            return true;
-        } else {
             try {
-                Thread.sleep(100);
+                if (ticket <= 0) {
+                    System.out.println("电影票出售完毕!");
+                    break;
+                } else {
+                        Thread.sleep(100);
+                    ticket--;
+                    System.out.println(Thread.currentThread().getName() + ": 在卖票, 还剩下: " + ticket);
+                }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
+            }finally {
+                lock.unlock();
             }
-            ticket--;
-            System.out.println(Thread.currentThread().getName() + ": 在卖票, 还剩下: " + ticket);
+
         }
-        return false;
     }
+
 }
